0f96165824152b9f7c9222b819980c413ad4e285
AKrendyasov <akrendyasov@gmail.com>
2019-09-30 18:20:58 +0300

    7 files modified, 1 added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe ('Test name', 'Some test description', 'https://ghost.nehalist.io/content/images/2017/09/angular-logo-1.png')
+  ]
   constructor() { }
 
   ngOnInit() {
