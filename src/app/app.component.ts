ee9a7b8ea001e57200c9f6107388ac58771f41bc
AKrendyasov <akrendyasov@gmail.com>
2019-10-03 11:24:31 +0300

    Navigation start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  loadedFeature: string = 'recipe'
+  constructor() { }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature
+  }
 }
