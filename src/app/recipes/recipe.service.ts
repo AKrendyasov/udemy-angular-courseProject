e3fd1ddb29595861225dd52d7914acff496055e3
AKrendyasov <akrendyasov@gmail.com>
2019-12-11 12:36:43 +0300

    118 Service cross recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter} from '@angular/core'
+
 import { Recipe } from './recipe.model'
 
 export class RecipeService {
+  recipeSelected = new EventEmitter <Recipe> ()
   private recipes: Recipe[] = [
     new Recipe ('Test recipe name', 'Some recipe test description', 'https://ghost.nehalist.io/content/images/2017/09/angular-logo-1.png'),
     new Recipe ('Test recipe name2', 'Some recipe test description2', 'https://ghost.nehalist.io/content/images/2017/09/angular-logo-1.png')
