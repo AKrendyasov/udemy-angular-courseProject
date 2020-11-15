44af54dd4f693867b81c46fd670fa849df520331
Raich <Raichiel@gmail.com>
2020-11-15 21:13:24 +0300

    298

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, Subject, throwError } from 'rxjs';
+import { UserModel } from './user.model';
 
 export interface AuthResponseData {
     idToken: string,
@@ -13,61 +14,77 @@ export interface AuthResponseData {
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
+    user = new Subject<UserModel>();
 
   constructor(private http: HttpClient) { }
 
-  singUp(email: string, password: string) {
-    const params = {
-      email,
-      password,
-      returnSecureToken: true
-    };
-    return this.http
-        .post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCfiVfPggCJTv_86Hwidn3CxsV3vCcFrGk',
-            params
-        )
-        .pipe(
-            catchError(this.handleError)
-        );
-  }
-  login(email: string, password: string) {
-    const params = {
-      email,
-      password,
-      returnSecureToken: true
-    };
-    return this.http
-        .post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCfiVfPggCJTv_86Hwidn3CxsV3vCcFrGk',
-            params
-        )
-        .pipe(
-            catchError(this.handleError)
-        );
-  }
+    singUp(email: string, password: string) {
+        const params = {
+            email,
+            password,
+            returnSecureToken: true
+        };
+        return this.http
+            .post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCfiVfPggCJTv_86Hwidn3CxsV3vCcFrGk',
+                params
+            )
+            .pipe(
+                catchError(this.handleError),
+                tap(resData => {
+                    this.handleAuthentication(
+                        resData.email,
+                        resData.localId,
+                        resData.idToken,
+                        +resData.expiresIn
+                    );
+                })
+            );
+    }
 
-  private handleError (error: HttpErrorResponse) {
-      let errorMessage = 'Unknown error '
-      switch (error?.error?.error?.message) {
-          case 'EMAIL_EXISTS':
-              errorMessage = 'This email already exists';
-              break;
-          case 'EMAIL_NOT_FOUND':
-              errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted';
-              break;
-          case 'INVALID_PASSWORD':
-              errorMessage = 'The password is invalid or the user does not have a password';
-              break;
-          case 'USER_DISABLED':
-              errorMessage = 'The user account has been disabled by an administrator';
-              break;
+    login(email: string, password: string) {
+        const params = {
+            email,
+            password,
+            returnSecureToken: true
+        };
+        return this.http
+            .post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCfiVfPggCJTv_86Hwidn3CxsV3vCcFrGk',
+                params
+            )
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
 
-      }
-      return throwError(errorMessage)
+    private handleAuthentication(email: string, userId: string, token: string, expiresAfter: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresAfter * 1000);
+        const user = new UserModel(email, userId, token, expirationDate);
+        this.user.next(user);
+    }
 
-  }
+    private handleError(error: HttpErrorResponse) {
+        let errorMessage = 'Unknown error ';
+        switch (error?.error?.error?.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'The password is invalid or the user does not have a password';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'The user account has been disabled by an administrator';
+                break;
+
+        }
+        return throwError(errorMessage);
+
+    }
 }
