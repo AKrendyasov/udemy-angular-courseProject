94205c534aa60d75e77118e861e368b998420fc9
Raich <Raichiel@gmail.com>
2020-11-18 23:29:19 +0300

    329, 330

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -10,7 +10,7 @@ import { RecipesResolverGuard } from './recipes-resolver.guard';
 
 const routes: Routes = [
   {
-    path: 'recipes',
+    path: '',
     component: RecipesComponent,
     canActivate: [AuthGuard],
     children: [
